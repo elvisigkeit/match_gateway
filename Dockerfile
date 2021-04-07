af9963b93194414ac4997154f255d2efa9f3b771
Elvis Nobrega <elvismdnin@gmail.com>
2021-04-07 16:49:52 -0300

    Reorganizing folders

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,22 @@
-FROM golang:alpine
-ENV GOPATH ""
+FROM golang:alpine AS builder
 
-ADD . .
+ENV GO111MODULE=on \
+    CGO_ENABLED=0 \
+    GOOS=linux \
+    GOARCH=amd64
+
+WORKDIR /build
+
+COPY . .
 
 RUN go get -d -v ./...
 RUN go install -v ./...
 
-ENTRYPOINT $HOME/go/bin/matchManager
+RUN ls
+
+FROM scratch
+
+COPY --from=builder /dist/match_gateway /
+
 EXPOSE 8000
+CMD ["/match_gateway"]
--- web/game_api.go
+++ web/game_api.go
@@ -1,9 +1,14 @@
 package web
 
 import (
+	"context"
 	"fmt"
 	"github.com/elvismdnin/match_gateway/internal/web"
 	"net/http"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 
 	"github.com/gorilla/mux"
 )
@@ -14,13 +19,26 @@ func CreateGame() web.Handler {
 			r.Path("/new").Methods("GET")
 		},
 		Func: func(w http.ResponseWriter, r *http.Request) {
-			var encodeErr error
+			config, err := rest.InClusterConfig()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			clientset, err := kubernetes.NewForConfig(config)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			for _, pod := range pods.Items {
+				println("Pod: ", pod.Spec.NodeName, " - ", pod.Status.StartTime)
+			}
+			fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
 			_, _ = fmt.Fprint(w, "{success}")
-
-			if encodeErr != nil {
-				http.Error(w, encodeErr.Error(), http.StatusInternalServerError)
-			}
 		},
 	}
 }
